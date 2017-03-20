63640cc9f590373f407ec2672ef5d3df04a478e6
Galymzhan Abdugalimov <abdu.galymzhan@gmail.com>
2017-03-20 17:32:25 +0600

    fixed jquery name in import section of tests

--- app/tests/components/Clock.test.jsx
+++ app/tests/components/Clock.test.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import expect from 'expect';
-import $ from 'jQuery';
+import $ from 'jquery';
 import TestUtils from 'react-addons-test-utils';
 
 import Clock from 'Clock';
