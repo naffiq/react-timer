e415ded8d0ce9ff6c527d6c4112af6bcab22d06b
Galymzhan Abdugalimov <abdu.galymzhan@gmail.com>
2017-03-27 22:57:24 +0600

    Tested countdown

---
+++ app/tests/components/Countdown.test.jsx
@@ -0,0 +1,49 @@
+import expect from 'expect';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import $ from 'jquery';
+import TestUtils from 'react-addons-test-utils';
+
+import Countdown from 'Countdown';
+
+describe('Countdown', () => {
+  it('should exist', () => {
+    expect(Countdown).toExist();
+  });
+
+  describe('handleSetCountdown', () => {
+    it('should set state to started and countdown', (done) => {
+      let countdown = TestUtils.renderIntoDocument(<Countdown/>);
+      countdown.handleSetCountdown(10);
+
+      expect(countdown.state.count).toBe(10);
+      expect(countdown.state.countdownStatus).toBe('started');
+
+      setTimeout(() => {
+        expect(countdown.state.count).toBe(9);
+        done();
+      }, 1001);
+    });
+
+    it('should not count down to negative values', (done) => {
+      let countdown = TestUtils.renderIntoDocument(<Countdown/>);
+      countdown.handleSetCountdown(1);
+
+      setTimeout(() => {
+        expect(countdown.state.count).toBe(0);
+        done();
+      }, 2001);
+    });
+
+    it('should set state to stopped after finished', (done) => {
+      let countdown = TestUtils.renderIntoDocument(<Countdown/>);
+      countdown.handleSetCountdown(1);
+
+      setTimeout(() => {
+        expect(countdown.state.count).toBe(0);
+        expect(countdown.state.countdownStatus).toBe('stopped');
+        done();
+      }, 2001);
+    });
+  });
+});
